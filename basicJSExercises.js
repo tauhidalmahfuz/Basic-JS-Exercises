073a2d011b8e48280fae052bc00a433868a5ceda
cis101 <cis101@secs.iub.edu.bd>
2017-09-27 09:08:05 +0600

    assignment 4

--- basicJSExercises.js
+++ basicJSExercises.js
@@ -13,13 +13,13 @@ Output:
  --------------------------- */
 
 function areaTriangle(base, height) {
-  return "No code yet";
+  return (1/2)*base*height;
 }
 console.log("Triangle Area Calculator:");
 /* Uncomment the following to check */
-  // console.log(areaTriangle(2, 7));
-  // console.log(areaTriangle(20, 56.5));
-  // console.log(areaTriangle(50, 34));
+  console.log(areaTriangle(2, 7));
+  console.log(areaTriangle(20, 56.5));
+  console.log(areaTriangle(50, 34));
 
 
 
@@ -44,12 +44,19 @@ HINT: Use substring()
  --------------------------- */
 
 function rotate_string(text) {
-  console.log(text);
+  var len= text.length;
+  for(var i=0;i<len;i++)
+  {
+    text=text.substring(1,len)+text[0];
+    console.log(text);
+  }
+
+
 }
 
 console.log("Rotate String:");
 /* Uncomment the following to check */
-  //rotate_string("cat");
+  rotate_string("cat");
   //rotate_string("voracious")
 
 
